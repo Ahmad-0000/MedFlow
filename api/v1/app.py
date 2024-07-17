6e2b0c6908bfbf0aab678d364466cffce8fd87d5
Ahmad <ahmad.new.m.v@gmial.com>
2024-07-17 23:32:16 +0200

    Main api app

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Main API app
+"""
+from flask import Flask, jsonify, make_response
+from api.v1.views import app_views
+from models import storage
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app, resorces={"*": {'origins': "*"}})
+
+@app.errorhandler(404)
+def not_found(error):
+    err_message = {'error': 'not found'}
+    return make_response(jsonify(err_message), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    err_message = {'error': error.description}
+    return make_response(jsonify(err_message), 400)
+
+@app.teardown_appcontext
+def refresh(exception):
+    storage.close()
