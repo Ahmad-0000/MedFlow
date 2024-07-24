debdf7c21807c6f2f1327c74757785bbda83a06f
Ahmad <ahmad.new.m.v@gmial.com>
2024-07-24 13:21:58 +0200

    Added documentations, and start using gunicorn

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,21 +13,22 @@ CORS(app, resorces={"*": {'origins': "0.0.0.0"}})
 
 @app.errorhandler(400)
 def bad_request(error):
+    """Handles 400 errors"""
     err_message = {'error': error.description}
     return make_response(jsonify(err_message), 400)
 
 @app.errorhandler(401)
 def unauthorized(error):
+    """Handles 401 errors"""
     return make_response(jsonify({'error': 'unauthorized'}), 401)
 
 @app.errorhandler(404)
 def not_found(error):
+    """Hanldes 404 errors"""
     err_message = {'error': 'not found'}
     return make_response(jsonify(err_message), 404)
 
 @app.teardown_appcontext
 def refresh(exception):
+    """Renews database session after each serve"""
     storage.close()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
