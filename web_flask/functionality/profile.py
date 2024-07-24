73f940da37971ac4028aa7b3c77398ed2bfe807a
Ahmad <ahmad.new.m.v@gmial.com>
2024-07-24 14:05:05 +0200

    Removed a but, prevented caching and improved readability
    Used 'credential_user' method instead of 'check_user' to check if the
    user with both the supplied email and password exists

--- web_flask/functionality/profile.py
+++ web_flask/functionality/profile.py
@@ -17,41 +17,41 @@ def user_profile(user_id):
     is_owner = None
     if not owner:
         abort(404)
-    q = len(owner.questions)
-    a = len(owner.answers)
+    qu_num = len(owner.questions)
+    an_num = len(owner.answers)
     status = request.cookies.get("status", None)
-    v_id = request.cookies.get("id", None)
+    visiter_id = request.cookies.get("id", None)
     email = request.cookies.get("email", None)
     password = request.cookies.get("password", None)
-    if not v_id or not email or not password:
+    if not visiter_id or not email or not password:
         is_owner = False
         return render_template('profile.html', user=owner, status=status,
-                               questions=q, answers=a, is_owner=is_owner)
-    id_visiter = storage.get(User, v_id)
-    credentail_visiter = storage.check_user(email, password, "h")
+                               questions=qu_num, answers=an_num, is_owner=is_owner, cache_id=cache_id)
+    id_visiter = storage.get(User, visiter_id)
+    credentail_visiter = storage.credential_user(email, password, "h")
     if not id_visiter:
         is_owner = False
         return render_template('profile.html', user=owner, status=status,
-                               questions=q, answers=a, is_owner=is_owner)
+                               questions=qu_num, answers=an_num, is_owner=is_owner, cache_id=cache_id)
     if not credentail_visiter:
         is_owner = False
         return render_template('profile.html', user=owner, status=status,
-                               questions=q, answers=a, is_owner=is_owner)
+                               questions=qu_num, answers=an_num, is_owner=is_owner, cache_id=cache_id)
     if credentail_visiter.to_dict() != id_visiter.to_dict():
         is_owner = False
         return render_template('profile.html', user=owner, status=status,
-                               questions=q, answers=a, is_owner=is_owner)
+                               questions=qu_num, answers=an_num, is_owner=is_owner, cache_id=cache_id)
     if id_visiter.to_dict() == owner.to_dict():
         is_owner = True
     return render_template('profile.html', user=owner, status=status,
-                           questions=q, answers=a, is_owner=is_owner)
+                           questions=qu_num, answers=an_num, is_owner=is_owner, cache_id=cache_id)
 
 
 @flask_app.route("/medflow/update_bio/<uuid:user_id>", strict_slashes=False,
                  methods=['GET'])
 def udpate_bio(user_id):
     """Render the template for updating the bio"""
-    return render_template("update_bio.html", user_id=user_id)
+    return render_template("update_bio.html", user_id=user_id, cache_id=cache_id)
 
 
 @flask_app.route("/medflow/update_bio_handler/<uuid:user_id>",
@@ -59,6 +59,9 @@ def udpate_bio(user_id):
 def update_bio_handler(user_id):
     """Update the user bio"""
     owner_id = str(user_id)
+    owner = storage.get(User, owner_id)
+    if not owner:
+        abort(404)
     status = request.cookies.get("status", None)
     visiter_id = request.cookies.get("id", None)
     v_email = request.cookies.get("email", None)
@@ -67,11 +70,8 @@ def update_bio_handler(user_id):
         abort(403, 'err_logging')
     if not visiter_id or not v_email or not v_password:
         abort(403, 'err_registeration')
-    owner = storage.get(User, owner_id)
     id_visiter = storage.get(User, visiter_id)
-    credentail_visiter = storage.check_user(v_email, v_password, "h")
-    if not owner:
-        abort(404)
+    credentail_visiter = storage.credential_user(v_email, v_password, "h")
     if not id_visiter or not credentail_visiter:
         abort(403, 'err_registeration')
     if id_visiter.to_dict() != credentail_visiter.to_dict():
@@ -87,7 +87,7 @@ def update_bio_handler(user_id):
                  methods=['GET'])
 def udpate_edu(user_id):
     """Render the template for updating the education info"""
-    return render_template("update_edu.html", user_id=user_id)
+    return render_template("update_edu.html", user_id=user_id, cache_id=cache_id)
 
 
 @flask_app.route("/medflow/update_edu_handler/<uuid:user_id>",
@@ -95,23 +95,23 @@ def udpate_edu(user_id):
 def update_edu_handler(user_id):
     """Update the user edu"""
     owner_id = str(user_id)
+    owner = storage.get(User, owner_id)
+    if not owner:
+        abort(404)
     status = request.cookies.get("status", None)
     visiter_id = request.cookies.get("id", None)
     v_email = request.cookies.get("email", None)
     v_password = request.cookies.get("password", None)
     if not status or status == "out":
-        return make_response(render_template('logfirst.html'), 403)
+        return make_response(render_template('logfirst.html', cache_id=cache_id), 403)
     if not visiter_id or not v_email or not v_password:
-        return make_response(render_template('regfirst.html'), 403)
-    owner = storage.get(User, owner_id)
+        return make_response(render_template('regfirst.html', cache_id=cache_id), 403)
     id_visiter = storage.get(User, visiter_id)
-    credentail_visiter = storage.check_user(v_email, v_password, "h")
-    if not owner:
-        abort(404)
+    credentail_visiter = storage.credential_user(v_email, v_password, "h")
     if not id_visiter or not credentail_visiter:
-        return make_response(render_template('regfirst.html'), 403)
+        return make_response(render_template('regfirst.html', cache_id=cache_id), 403)
     if id_visiter.to_dict() != credentail_visiter.to_dict():
-        return make_response(render_template("userconflict.html"), 409)
+        return make_response(render_template("userconflict.html", cache_id=cache_id), 409)
     if id_visiter.id != owner_id:
         abort(401)
     edu = request.form.get('body')
